ce1d3c66d5cab30743e44f21283eff1ce88a4372
Björn Mosler <dev@bjoern.mosler.ch>
2019-11-13 13:30:12 +0100

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Raphael from 'raphael';
+
+const r = new Raphael("frame-geo-viz", 200, 200)
+
+r.circle(50, 40, 10);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: 'frame-geo-viz',
+      mobile: true
+    }),
+  ]
+};
